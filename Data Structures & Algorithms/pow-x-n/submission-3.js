1f6aaf33440bc3a9af3bd106001c23ed26e02cf7
Ajay Barman <barmanjiaj@gmail.com>
2026-04-29 01:41:22 +0530

    Add: pow-x-n - submission-3

---
+++ Data Structures & Algorithms/pow-x-n/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @param {number} n
+     * @return {number}
+     */
+    myPow(x, n) {
+  if (n === 0) return 1;
+  let res = 1;
+  const iterations = Math.abs(n);
+
+  for (let i = 0; i < iterations; i++) {
+    if (n > 0) {
+      res *= x;
+    } else {
+      res /= x;
+    }
+  }
+return res
+    }
+}
