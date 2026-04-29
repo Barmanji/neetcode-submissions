7280c44a99c49a2dd576cc6188bf9dde4a502dfe
Ajay Barman <barmanjiaj@gmail.com>
2026-04-29 21:24:56 +0530

    Add: number-of-one-bits - submission-5

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-5.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        /**
+         * @param {number} n - a positive integer
+         * @return {number}
+         */
+        let res = 0;
+        for (let i = 0; i < 32; i++) {
+            if ((1 << i) & n) { // essentially increasing 1's place ahead like 001 -> 010 -> 100 to & with n if 1 returns which is true means resulat add simple
+                res++;
+            }
+        }
+        return res;
+    }
+}
