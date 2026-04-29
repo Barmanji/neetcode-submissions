47c0dee456f0205ba8893dd469d0a68acc6bd66e
Ajay Barman <barmanjiaj@gmail.com>
2026-04-29 21:24:12 +0530

    Add: number-of-one-bits - submission-4

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number}
+     */
+    hammingWeight(n) {
+        /**
+         * @param {number} n - a positive integer
+         * @return {number}
+         */
+        let res = 0;
+        for (let i = 0; i < 32; i++) {
+            if ((1 << i) & n) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
